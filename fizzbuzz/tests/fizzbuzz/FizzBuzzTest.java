6dac4b8c902b13eadf45393160c32f67e3d94ed7
Terry Yin <terry@odd-e.com>
2014-06-09 16:06:21 +0800

    fizzbuzz exercise

---
+++ fizzbuzz/tests/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,27 @@
+package fizzbuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+	
+	@Before
+	public void setUp() throws Exception {
+		// will run before each test
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// will run after each test.
+		// even when the test fails.
+	}
+
+	@Test
+	public void test() {
+		assertEquals(1, 2);
+	}
+
+}
